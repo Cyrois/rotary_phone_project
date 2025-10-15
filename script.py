a9cddd8eb5aafb1fee66f1d340860bc3cb0b8276
Calvin Chan <cyrois3@gmail.com>
2025-10-14 20:31:32 -0700

    add all initial files

---
+++ script.py
@@ -0,0 +1,18 @@
+# import required module
+from playsound import playsound
+import time
+from datetime import datetime
+
+
+latestTweetIdFile = "latestTweetId.txt"
+
+#Run the Loop
+while True:
+    #open a sound file
+    #Buffer the audio stream
+    #Play Audio Buffer
+    #Close the Sound file?
+
+    # for playing note.wav file
+    playsound('note.wav')
+    print('playing sound using  playsound')
